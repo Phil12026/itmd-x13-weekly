bb0124d22a147db73371d87664ec2f350b54b6e5
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 09:25:49 -0500

    removed debuging line since that section of code checksout.

--- pine/exercise2/ImproveFileRename.rb
+++ pine/exercise2/ImproveFileRename.rb
@@ -4,7 +4,6 @@ Dir.chdir './photosfolder'
 photos = Dir['/home/dnelson/Documents/itmd-x13-weekly/pine/exercise2/photosfolder/*.pic']
 puts "Give a name for this group of photos."
 groupname = gets.chomp
-puts photos.to_s
 
 puts "Photos in folder are as follows."
 photos.each do |absolutePath|
