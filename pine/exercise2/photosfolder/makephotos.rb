bd43dfc121b361358325b6ea13479ac207fa9e06
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 14:21:44 -0500

    added .to_s to x in both naming conventions now.

--- pine/exercise2/photosfolder/makephotos.rb
+++ pine/exercise2/photosfolder/makephotos.rb
@@ -1,7 +1,7 @@
 x = 0
 until x == 200
   File.open "phtoto" + x.to_s + ".pic",'w' do |make|
-    contents = "This file represents photo " + x + " for the assignment"
+    contents = "This file represents photo " + x.to_s + " for the assignment"
     f.write contents
   end
   x += 1
