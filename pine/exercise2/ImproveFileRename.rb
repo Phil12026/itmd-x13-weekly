8524288e442bf06b54b5e4d93cb329fedae5c22c
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 16:16:30 -0500

    fixed couple bugs.

--- pine/exercise2/ImproveFileRename.rb
+++ pine/exercise2/ImproveFileRename.rb
@@ -3,14 +3,14 @@ require 'yaml'
 def grabPhotos
   Dir.chdir './photosfolder'
   photos = Dir['/home/dnelson/Documents/itmd-x13-weekly/pine/exercise2/photosfolder/*.pic']
-  puts "Give a name for this group of photos."
+  photos
 end
 
-def photoCount
-  puts "Photos in folder are as follows."
+def countPhotos photos
   if photos.count == 0
     puts "There are no Photos in the directory."
   elsif photos.count < 15
+    puts "Photos in folder are as follows."
     photos.each do |absolutePath|
       puts absolutePath
     end
@@ -20,6 +20,6 @@ def photoCount
   photos.count
 end
 
-grabPhotos
-photoesCount
+photos = grabPhotos
+countPhotos photos
 
