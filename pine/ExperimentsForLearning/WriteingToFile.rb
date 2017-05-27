f81d479759c390eb4a7a6eac73ba8de1ee6eb956
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 00:38:00 -0500

    fixed typo

--- pine/ExperimentsForLearning/WriteingToFile.rb
+++ pine/ExperimentsForLearning/WriteingToFile.rb
@@ -11,7 +11,7 @@ puts input
 puts (input == testing)
 
 puts "yaml part now"
-array[]
+array= []
 array.push "The author of this book,"
 array.push "tries WAY to hard to be funny,"
 array.push "and it ends up blowing up in his face."
