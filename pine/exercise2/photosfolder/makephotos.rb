1f1e78775439c3b95eeb2d2b2a67f9616d1ff494
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 14:22:43 -0500

    corrected what does the writing.

--- pine/exercise2/photosfolder/makephotos.rb
+++ pine/exercise2/photosfolder/makephotos.rb
@@ -2,7 +2,7 @@ x = 0
 until x == 200
   File.open "phtoto" + x.to_s + ".pic",'w' do |make|
     contents = "This file represents photo " + x.to_s + " for the assignment"
-    f.write contents
+    make.write contents
   end
   x += 1
 end
