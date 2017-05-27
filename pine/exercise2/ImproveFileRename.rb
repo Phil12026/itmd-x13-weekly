c2e096ec8f7a54876efc291888c1e971090278f1
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 15:21:15 -0500

    added logic for there being zero photos.

--- pine/exercise2/ImproveFileRename.rb
+++ pine/exercise2/ImproveFileRename.rb
@@ -6,10 +6,12 @@ puts "Give a name for this group of photos."
 groupname = gets.chomp
 
 puts "Photos in folder are as follows."
-if photos.count > 15
-  puts "there are " + photos.count.to_s + " number of photos."
-else
+if photos.count == 0
+  puts "There are no Photos in the directory."
+elsif photos.count < 15
   photos.each do |absolutePath|
     puts absolutePath
   end
+else
+  puts "there are " + photos.count.to_s + " photos."
 end
