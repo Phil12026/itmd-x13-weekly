9fcafcaca025182e21c01115793379b661e2d1dc
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 14:20:13 -0500

    changed f in file to uppercase.

--- pine/exercise2/photosfolder/makephotos.rb
+++ pine/exercise2/photosfolder/makephotos.rb
@@ -1,6 +1,6 @@
 x = 0
-until x = 200
-  file.open "phtoto" + x + ".pic", 'w' do |make|
+until x == 200
+  File.open "phtoto" + x + ".pic",'w' do |make|
     contents = "This file represents photo " + x + " for the assignment"
     f.write contents
   end
