b3c1758c1cc17391087f6fe471b752c190946a6f
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 16:10:00 -0500

    removed unneeded line.

--- pine/exercise2/ImproveFileRename.rb
+++ pine/exercise2/ImproveFileRename.rb
@@ -1,17 +1,25 @@
 require 'yaml'
 
-Dir.chdir './photosfolder'
-photos = Dir['/home/dnelson/Documents/itmd-x13-weekly/pine/exercise2/photosfolder/*.pic']
-puts "Give a name for this group of photos."
-groupname = gets.chomp
-
-puts "Photos in folder are as follows."
-if photos.count == 0
-  puts "There are no Photos in the directory."
-elsif photos.count < 15
-  photos.each do |absolutePath|
-    puts absolutePath
-  end
-else
-  puts "there are " + photos.count.to_s + " photos."
+def grabPhotos
+  Dir.chdir './photosfolder'
+  photos = Dir['/home/dnelson/Documents/itmd-x13-weekly/pine/exercise2/photosfolder/*.pic']
+  puts "Give a name for this group of photos."
 end
+
+def photoCount
+  puts "Photos in folder are as follows."
+  if photos.count == 0
+    puts "There are no Photos in the directory."
+  elsif photos.count < 15
+    photos.each do |absolutePath|
+      puts absolutePath
+    end
+  else
+    puts "there are " + photos.count.to_s + " photos."
+  end
+  photos.count
+end
+
+grabPhotos
+photoesCount
+
