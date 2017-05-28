d813c38055d468dd3872a0d215f07dfd87eeab28
Dean Nelson <DeanNelson.IT@gmail.com>
2017-05-27 20:24:22 -0500

    started cut/copy method.

--- pine/exercise2/ImproveFileRename.rb
+++ pine/exercise2/ImproveFileRename.rb
@@ -23,9 +23,26 @@ def whereToStore
   puts "What folder do you want to store the photos in?"
   userfolder = gets.chomp
   check = Dir.exist? "/home/dnelson/Pictures/#{userfolder}" 
+  if check == false
+    Dir.mkdir userfolder
+  end
+  userfolder
+end
+
+def movePhotos photos userfolder
+  while true
+    puts "Do you want to cut or copy the photos?"
+    puts "Enter cut to delete original, and copy to save the original."
+    cutOrCopy = gets.chomp.upcase
+    if cutOrCopy == "CUT"
+      FileUtils.mv 
+    elsif
+    
+    end
+  end
 end
 
 photos = grabPhotos
 countPhotos photos
 whereToStore
-
+movePhotos photos whereToStore
