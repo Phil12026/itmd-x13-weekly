c851157f7d25819e35daba107389e2b73f85742e
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-03 11:42:38 -0500

    Reset depot rails config
    Reset back to fresh configuration for the Task A application to
    get rid of all the learing excersices.

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'say/hello'
-
-  get 'say/goodbye'
-
-  get 'say/hellomoto'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
